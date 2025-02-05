b5e6c2f13f4c539d003d73a60181943e9584b8bf
dakohhh <wiizzydreadmill@gmail.com>
2025-02-05 17:46:13 +0100

    cleared cache

--- configs/index.ts
+++ configs/index.ts
@@ -25,7 +25,7 @@ const DEPLOYMENT_ENV = process.env.NODE_ENV || "development";
 const GLOBAL_CONSTANTS = {
   // System Constants
   // ============================================================
-  APP_NAME: "",
+  APP_NAME: "Ascent",
   APP_DESCRIPTION: ".",
   SUPPORT_EMAIL: "",
   DEFAULT_EMAIL_FROM: "",
@@ -56,43 +56,14 @@ const GLOBAL_CONSTANTS = {
   ROLES: {
     USER: "user",
     SUPER_ADMIN: "super-admin",
-    CONTENT_MANAGER: "content-manager",
   },
-
-  AWS: {
-    AWS_REGION: process.env.AWS_REGION,
-    S3_BUCKET: process.env.AWS_S3_BUCKET_NAME,
-    ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
-    SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
-    CLOUD_FRONT_URL: process.env.AWS_CLOUD_FRONT_URL,
-  },
-  PAYSTACK: {
-    PAYSTACK_BASE_URL: process.env.PAYSTACK_BASE_URL || "https://api.paystack.co",
-    PAYSTACK_SECRET_KEY: process.env.PAYSTACK_SECRET_KEY,
-  },
-
-  STRIPE: {
-    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
-    STRIPE_PUBLIC_KEY: process.env.STRIPE_PUBLIC_KEY,
-  },
-
-  ALGOLIA: {
-    ALGOLIA_API_KEY: process.env.ALGOLIA_API_KEY,
-    ALGOLIA_APP_ID: process.env.ALGOLIA_APP_ID,
-  },
-
-  // APPLE: {
-  //   PRIVATE_KEY: process.env.APPLE_PRIVATE_KEY!,
-  //   TEAM_ID: process.env.APPLE_TEAM_ID!,
-  //   KEY_ID: process.env.APPLE_KEY_ID!,
-  //   CLIENT_ID: process.env.APPLE_CLIENT_ID!,
-  //   CALLBACK_URL: process.env.APPLE_CALLBACK_URL!,
-  // },
 };
 
 const CONFIG_BUILDER = {
   development: {
     ...GLOBAL_CONSTANTS,
+
+    HOST: "127.0.0.1",
 
     // System Constants
     // ============================================================
@@ -104,53 +75,26 @@ const CONFIG_BUILDER = {
 
     // Security / Auth Configs
     // ============================================================
-    JWT_SECRET: "T4u2Rcnne09F.FBr11f0VvERyUiq",
+    JWT_SECRET: "secret",
 
     // DB Constants
     // ============================================================
     REDIS_URI: "redis://127.0.0.1:6379",
-    MONGO_URI: "mongodb://localhost:27017/arewaflix",
-
-    // mongodb+srv://admin-dprince:%3Cdb_password%3E@cluster0.5uocq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-
-    // Mailer Configs
-    // ============================================================
-    MAILER: {
-      SMTP_HOST: "localhost",
-      SMTP_PORT: 2525,
-      SMTP_USER: "user",
-      SMTP_PASSWORD: "pass",
-      SECURE: false,
-      FROM_EMAIL: "ArewaFlix <no-reply@arewaflix.com>",
-    },
+    MONGO_URI: "mongodb://localhost:27017/ascent",
 
     // App Level Configs
     // ============================================================
     CORS_ALLOWED_ORIGINS: ["https://admin.socket.io", "http://localhost:3000"],
 
-    SOCKET_IO: {
-      USERNAME: "admin",
-      PASSWORD: "password",
-    },
-
     SWAGGER: {
       PATH: "/docs",
       PASSWORD: "password",
     },
-
-    // GOOGLE: {
-    //   CLIENT_ID: "418901255882-c61v4mn8p1a1lg3dnfng285mbah0sujf.apps.googleusercontent.com",
-    //   CLIENT_SECRET: "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",
-    //   CALLBACK_URL: "http://localhost:4000/v1/auth/google/callback",
-    // },
-
-    // STRIPE: {
-    //     PUBLIC_KEY: "pk_test_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    //     SECRET_KEY: "sk_test_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    // },
   },
 
   production: {
+    HOST: process.env.HOST || "0.0.0.0",
+
     ...GLOBAL_CONSTANTS,
 
     // System Constants
@@ -170,43 +114,19 @@ const CONFIG_BUILDER = {
     REDIS_URI: process.env.REDIS_URI!,
     MONGO_URI: process.env.MONGO_URI!,
 
-    // Mailer Configs
-    // ============================================================
-    MAILER: {
-      SMTP_HOST: process.env.MAILER_SMTP_HOST,
-      SMTP_PORT: process.env.MAILER_SMTP_PORT,
-      SMTP_USER: process.env.MAILER_SMTP_USER,
-      SMTP_PASSWORD: process.env.MAILER_SMTP_PASSWORD,
-      SECURE: process.env.MAILER_SECURE === "true" ? true : false,
-      USE_AWS_SES: process.env.MAILER_USE_AWS_SES === "true" ? true : false,
-      FROM_EMAIL: "EatNVibe <no-reply@eatandvibe.com>",
-    },
-
     // App Level Configs
     // ============================================================
     CORS_ALLOWED_ORIGINS: ["https://admin.socket.io", "https://example.com"],
 
-    SOCKET_IO: {
-      USERNAME: process.env.SOCKET_IO_USERNAME!,
-      PASSWORD: process.env.SOCKET_IO_PASSWORD!,
-    },
+    // SOCKET_IO: {
+    //   USERNAME: process.env.SOCKET_IO_USERNAME!,
+    //   PASSWORD: process.env.SOCKET_IO_PASSWORD!,
+    // },
 
     SWAGGER: {
       PATH: "/docs",
       PASSWORD: process.env.SWAGGER_PASSWORD!,
     },
-
-    GOOGLE: {
-      CLIENT_ID: process.env.GOOGLE_CLIENT_ID!,
-      CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET!,
-      CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL!,
-    },
-
-    // e.g
-    // STRIPE: {
-    //     PUBLIC_KEY: "pk_test_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    //     SECRET_KEY: "sk_test_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    // },
   },
 } as const;
 
